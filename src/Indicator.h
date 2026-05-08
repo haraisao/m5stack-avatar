5afb9a80bec0c1f38632f6016b1b09b878bcdbf9
Isao Hara <haraisao@gmail.com>
2026-05-08 15:08:30 +0900

    add flushing

--- src/Indicator.h
+++ src/Indicator.h
@@ -14,20 +14,21 @@ class Indicator final : public Drawable {
  private:
   const lgfx::IFont *font = nullptr;
   String text="";
+  bool flushing = true;
 
   void drawActiveMark(M5Canvas *spi, uint32_t x, uint32_t y, 
-                      uint16_t color, uint16_t bgColor) {
-    drawActiveMark(spi, x, y, color,bgColor, 0);
+                      uint16_t color, uint16_t bgColor, bool flushing) {
+    drawActiveMark(spi, x, y, color,bgColor, 0, flushing);
   }
 
   void drawActiveMark(M5Canvas *spi, uint32_t x, uint32_t y, 
-                      uint16_t color, uint16_t bgColor, float offset) {
+                      uint16_t color, uint16_t bgColor, float offset, bool flushing) {
     if (text.length() == 0) return;
     spi->setTextDatum(top_left);
     spi->setTextColor(color, bgColor);
     spi->setTextSize(2);
     //spi->fillRect(x, y, w, 20, bgColor);
-    if(std::abs(offset) < 0.5) return;
+    if(flushing && std::abs(offset) < 0.5) return;
     //M5_LOGI("Indicator(%d, %d)", x, y);
     if (font){
       spi->drawString(text.c_str(), x, y, font);
@@ -43,6 +44,9 @@ class Indicator final : public Drawable {
   ~Indicator() = default;
   Indicator(const Indicator &other) = default;
   Indicator &operator=(const Indicator &other) = default;
+  void setFlushing(bool v){
+    this->flushing = v;
+  }
   void draw(M5Canvas *spi, BoundingRect rect, DrawContext *ctx) override {
     uint16_t primaryColor = ctx->getColorDepth() == 1 ? 1 : ctx->getColorPalette()->get(COLOR_INFO_FOREGROUND);
     uint16_t bgColor = ctx->getColorDepth() == 1 ? ERACER_COLOR : ctx->getColorPalette()->get(COLOR_INFO_BACKGROUND);
@@ -52,7 +56,7 @@ class Indicator final : public Drawable {
 #if 1
     text = ctx->getInfoText();
     if (text != ""){
-      drawActiveMark(spi, 0, 0, primaryColor, bgColor, -offset);
+      drawActiveMark(spi, 0, 0, primaryColor, bgColor, -offset, this->flushing);
     }
 #else
     switch (exp) {
